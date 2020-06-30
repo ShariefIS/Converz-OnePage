84bd96b77e2e81b847d8bfcb2e63423750d1d923
SHARIEF <imadsharf@live.com>
2020-07-01 04:21:04 +0530

    click event on play button

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,7 +27,18 @@ window.addEventListener("scroll", event => {
         }
     });
 });
-
+; (function ($, win) {
+    $.fn.inViewport = function (cb) {
+        return this.each(function (i, el) {
+            function visPx() {
+                var H = $(this).height(),
+                    r = el.getBoundingClientRect(), t = r.top, b = r.bottom;
+                return cb.call(el, Math.max(0, t > 0 ? H - t : (b < H ? b : H)));
+            } visPx();
+            $(win).on("resize scroll", visPx);
+        });
+    };
+}(jQuery, window));
 $(document).ready(function () {
 
     $(window).scroll(function () {
@@ -44,5 +55,13 @@ $(document).ready(function () {
         }, 100);
         return false;
     });
+    $("#feature-fade").inViewport(function (px) {
+        if (px) $(this).addClass("fade-in");
+    });
 
 });
+function watchVideo() {
+    $(".placeholder").addClass("d-none");
+    $(".placeholder").removeClass("d-flex");
+    $("#dashboard-video").removeClass("d-none")
+}
